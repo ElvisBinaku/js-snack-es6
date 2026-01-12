fd730b6a644d77baf7f78814322e23b45fa37ce3
ElvisBinaku <elvisbinaku@icloud.com>
2026-01-12 23:37:25 +0100

    creato array di oggetti eltterali (squadre) creata funzione per generare un numero randomico per falli subiti e i punti fatti. Stampato in console  creando un if se i falli subiti sono più di 1.

--- snack-2/script.js
+++ snack-2/script.js
@@ -3,3 +3,69 @@
 //Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 //Generare numeri random al posto degli 0 nelle proprietà “punti fatti” e “falli subiti”.
 //Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+const squadre = [
+    {
+        nome: "venezia",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "inter",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "juve",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "napoli",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "roma",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "udinese",
+        puntiFatti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "treviso",
+        puntiFatti: 0,
+        falliSubiti: 0,
+        //per generare i numeri per i falli e punti mettendolo come metodo devo richiamarlo sempre sulla console
+        //random() {
+        /*for (let index = 0; index < squadre.length; index++) {
+        this.falliSubiti = Math.floor(Math.random() * 5);
+        this.puntiFatti = Math.floor(Math.random() * 30);
+        }
+        } */  
+    },
+    
+]
+
+const randomFalli = function() {
+for (let index = 0; index < squadre.length; index++) {
+squadre[index].puntiFatti = Math.floor(Math.random() * 30);
+squadre[index].falliSubiti = Math.floor(Math.random() * 7);
+}
+}
+
+randomFalli()
+
+for (let index = 0; index < squadre.length; index++) { 
+    let singPlur = "";
+    if (squadre[index].falliSubiti > 1){
+        singPlur = "falli"
+    } else {
+        singPlur = "fallo"
+    }
+    console.log(`${squadre[index].nome} ha subito ${squadre[index].falliSubiti} ${singPlur}`)
+}
+   
