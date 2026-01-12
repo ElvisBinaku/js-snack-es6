4b1af7266ae91b8362417cc3c8abd5f5e70479c2
ElvisBinaku <elvisbinaku@icloud.com>
2026-01-13 00:30:43 +0100

    creato novo array con map

--- snack-2/script.js
+++ snack-2/script.js
@@ -2,7 +2,6 @@
 //Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
 //Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 //Generare numeri random al posto degli 0 nelle proprietà “punti fatti” e “falli subiti”.
-//Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 
 const squadre = [
     {
@@ -41,31 +40,36 @@ const squadre = [
         falliSubiti: 0,
         //per generare i numeri per i falli e punti mettendolo come metodo devo richiamarlo sempre sulla console
         //random() {
-        /*for (let index = 0; index < squadre.length; index++) {
-        this.falliSubiti = Math.floor(Math.random() * 5);
-        this.puntiFatti = Math.floor(Math.random() * 30);
+            /*for (let index = 0; index < squadre.length; index++) {
+                this.falliSubiti = Math.floor(Math.random() * 5);
+                this.puntiFatti = Math.floor(Math.random() * 30);
+                }
+                } */  
+            },
+            
+        ]
+        
+        const randomFalli = function() {
+            for (let index = 0; index < squadre.length; index++) {
+                squadre[index].puntiFatti = Math.floor(Math.random() * 30);
+                squadre[index].falliSubiti = Math.floor(Math.random() * 7);
+            }
         }
-        } */  
-    },
-    
-]
-
-const randomFalli = function() {
-for (let index = 0; index < squadre.length; index++) {
-squadre[index].puntiFatti = Math.floor(Math.random() * 30);
-squadre[index].falliSubiti = Math.floor(Math.random() * 7);
-}
-}
-
-randomFalli()
-
-for (let index = 0; index < squadre.length; index++) { 
-    let singPlur = "";
-    if (squadre[index].falliSubiti === 1){
-        singPlur = "fallo"
-    } else {
-        singPlur = "falli"
-    }
-    console.log(`${squadre[index].nome} ha subito ${squadre[index].falliSubiti} ${singPlur}`)
-}
-   
+        
+        randomFalli()
+        
+        for (let index = 0; index < squadre.length; index++) { 
+            
+            let singPlur = "";
+            if (squadre[index].falliSubiti === 1){
+                singPlur = "fallo"
+            } else {
+                singPlur = "falli"
+            }
+            console.log(`${squadre[index].nome} ha subito ${squadre[index].falliSubiti} ${singPlur}`)
+        }
+        
+        
+        //Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+        const newArray = squadre.map(({ nome, falliSubiti }) => ({ [nome]: falliSubiti }));
+        console.log(newArray)
